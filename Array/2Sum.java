9033977bffa8bcab3592d3378ee3c829f7d28c6e
aditya6875 <ag2k1617@gmail.com>
2023-01-19 16:49:54 +0530

    Sort012

--- Array/2Sum.java
+++
@@ -1,6 +0,0 @@
-public class 2Sum {
-    public static void main(String[] args) {
-        int[] arr= {2,3,4,5,6,7};
-        System.out.println(arr);
-    }
-}
---
+++ Array/Sort012.java
@@ -0,0 +1,33 @@
+public class Sort012 {
+    public static void main(String[] args) {
+        int[] arr={0,1,2,2,1,0,0};
+        int low=0;
+        int mid=0;
+        int high=arr.length-1;
+        int n=high-1;
+        while(mid<=high){
+            if(arr[mid]==1){
+                mid++;
+            }
+            else if(arr[mid]==0){
+                int temp=arr[mid];
+                arr[mid]=arr[low];
+                arr[low]=temp;
+                mid++;
+                low++;
+            }
+            else{
+                int temp=arr[mid];
+                arr[mid]=arr[high];
+                arr[high]=temp;
+                high--;
+                ;
+            }
+        }
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+            
+        }
+    }
+}
